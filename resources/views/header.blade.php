8fec270458d8ebd3a22814920a3626a494ca431b
Abhilasha-Duwal <abhilashaduwal@gmail.com>
2021-06-12 18:03:59 +0545

    brand logo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -15,7 +15,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Ecommerce Project</a>
+      <a class="navbar-brand" href="#">Ecommerce</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
